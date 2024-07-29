d902f9831464b3a32dbf6f2cea6c6ec6658e508b
Ishara Madushan <IT21183904@my.sliit.lk>
2024-07-30 01:02:11 +0530

    chore: Refactor server.js for better code organization and formatting

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
-import productRouter from './routes/ProductRouter.js';
-import categoryRoutes from './routes/categoryRoutes.js';
-import infoRoutes from './routes/infoRoutes.js'; 
-import orderRoutes from './routes/orderRoutes.js';
-import accountSettings from './routes/accountSettingsRoutes.js'
-import addressRoutes from './routes/addressRoutes.js';
-import phoneNumberRoutes from './routes/phoneNumberRoutes.js';
-
-
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import productRouter from "./routes/ProductRouter.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import infoRoutes from "./routes/infoRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import accountSettings from "./routes/accountSettingsRoutes.js";
+import addressRoutes from "./routes/addressRoutes.js";
+import phoneNumberRoutes from "./routes/phoneNumberRoutes.js";
 
 // Create express app
 const app = express();
@@ -22,38 +20,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // API test route
-app.get('/api/hello', (req, res) => {
-  res.send('Hello World');
+app.get("/api/hello", (req, res) => {
+  res.send("Hello World");
 });
 
 //connect to mongodb
 dotenv.config();
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { });
-
-app.use('/api/products', productRouter);
-
-
-
+mongoose.connect(uri, {});
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
 });
 
 // Authentication routes
-app.use('/api/user', userRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/user',accountSettings);
-app.use('/api/address', addressRoutes);
-app.use('/api/phonenumber', phoneNumberRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", accountSettings);
+app.use("/api/address", addressRoutes);
+app.use("/api/phonenumber", phoneNumberRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/products", productRouter);
 
 // Use category routes
-app.use('/categories', categoryRoutes); // Add this line
-app.use('/info', infoRoutes);
-
-// Use routes
-app.use('/orders', orderRoutes);
+app.use("/categories", categoryRoutes); // Add this line
+app.use("/info", infoRoutes);
 
 // Start app
 const PORT = process.env.PORT || 5000;

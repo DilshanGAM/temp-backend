20dda042e566d1f550a66b281ee54834e0c13a71
Sajith Jayasooriya <It22280138@my.sliit.lk>
2024-08-29 19:16:33 +0530

    order route update with temps

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,10 +10,21 @@ import authenticate from "../middlewares/authenticate.js";
 
 const router = express.Router();
 
+
+router.post("/temp", createOrder);
+router.get("/temp", getOrders);
+router.get("/temp/:id", getOrderById);
+router.put("/temp/:id", updateOrder);
+router.delete("/temp/:id", deleteOrder);
+
 router.post("/", authenticate, createOrder); // POST /orders
 router.get("/", authenticate, getOrders); // GET /orders
 router.get("/:id", authenticate, getOrderById); // GET /orders/:id
 router.put("/:id", authenticate, updateOrder); // PUT /orders/:id
 router.delete("/:id", authenticate, deleteOrder); // DELETE /orders/:id
+
+
+
+
 
 export default router;

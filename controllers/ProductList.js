c606b7e4148bb22419844c6d2b18ae9718af21b7
Sajith Jayasooriya <It22280138@my.sliit.lk>
2024-09-11 09:49:41 +0530

    fixed produt page limit

--- controllers/ProductList.js
+++ controllers/ProductList.js
@@ -21,7 +21,7 @@ export const addProduct = async (req, res) => {
 // 10 Products per page
 export const getAllProduct = async (req, res) => {
   const page = parseInt(req.body.page) || 1;
-  const limit = parseInt(req.body.limit) || 10;
+  const limit = parseInt(req.body.limit) || 12;
   const skip = (page - 1) * limit;
   try {
     const products = await productList.find().skip(skip).limit(limit);
